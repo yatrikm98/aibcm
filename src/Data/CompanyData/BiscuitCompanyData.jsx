24734cb87fbe55ecd3a3e3885604f3700dc12aaf
Yatrik <yatrikmehta82@gmail.com>
2026-07-13 15:17:51 +0530

    case sensitive error in biscuit company datajsx

--- src/Data/CompanyData/BiscuitCompanyData.jsx
+++ src/Data/CompanyData/BiscuitCompanyData.jsx
@@ -111,7 +111,7 @@ const BiscuitCompanyData = [
         "ImageButtonTitle": "Disha-Foods",
         "images": [
             BonTon,
-            BourBon,
+            Bourbon,
             Bro,
             BroPop,
             ButterMate
